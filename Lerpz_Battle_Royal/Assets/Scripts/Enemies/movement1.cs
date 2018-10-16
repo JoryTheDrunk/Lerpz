b554837afa4d08ab8162df0c2b8e7bf2b16cbbe9
Joey <43388262+JoryTheDrunk@users.noreply.github.com>
2018-10-16 16:50:17 -0500

    Adding Assets

---
+++ Lerpz_Battle_Royal/Assets/Scripts/Enemies/movement1.cs
@@ -0,0 +1,75 @@
+﻿//-JAM
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AddComponentMenu("(Enemy)/Turn to face and then chase the player.")]
+
+public class movement1 : MonoBehaviour {
+
+public float spd0 = 0;		//basic movement speed
+public float spd1 = 0;		//sidestep movement speed
+public float rot0 = 0;		//rotation speed
+private float x = 0;
+private float z = 0;
+public float range0 = 0;	//how far the enemy can see
+public float range1 = 0;	//how far the enemy checks for a wall (sideways)
+private Transform target0;	//the player target
+private bool enter = false;	//is the player near?
+private RaycastHit hit0;	//what the raycast hits
+private RaycastHit hit1;	//what the raycast hits
+
+	void Start(){
+		var temp0 = GameObject.FindWithTag("Player");
+		target0 = temp0.GetComponent<Transform>();
+	}
+
+	void Update(){
+			//if the player enters the enemy's range of ditection (collider)
+		if(enter == true){
+				//set the rotation to target the player
+			Vector3 turn0 = target0.position - transform.position;
+				//set the targeted rotation speed
+			float step0 = rot0 * Time.deltaTime;
+				//follow the player transform
+			Vector3 newDir0 = Vector3.RotateTowards(transform.forward, turn0, step0, 0.0f);
+				//rotate towards the player
+			transform.rotation = Quaternion.LookRotation(newDir0);
+		}
+			//send out a raycast to see if the player is in front of the enemy
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit0, range0) && hit0.transform.tag == "Player"){
+				//move towards the player
+			transform.Translate(Vector3.forward * spd0 * Time.deltaTime);
+		}
+			//raycasts to the right
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit1, range1) && hit1.transform.tag == "Wall"){
+				//sidesteps to the left, away from the wall
+			transform.Translate(-Vector3.right * spd1 * Time.deltaTime);
+		}	//raycasts to the left (-right)
+		if(Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.right), out hit1, range1) && hit1.transform.tag == "Wall"){
+				//sidesteps to the right, away from the wall
+			transform.Translate(Vector3.right * spd1 * Time.deltaTime);
+		}
+			//debug lines, drawn to show the raycasts
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * range0;
+        Debug.DrawRay(transform.position, forward, Color.red);
+		Vector3 left = transform.TransformDirection(-Vector3.right) * range1;
+        Debug.DrawRay(transform.position, left, Color.green);
+		Vector3 right = transform.TransformDirection(Vector3.right) * range1;
+        Debug.DrawRay(transform.position, right, Color.green);
+	}
+
+	void OnTriggerEnter(Collider other){
+			//when the player triggers the collider
+		if(other.gameObject.tag == "Player"){
+			enter = true;
+		}
+	}
+	void OnTriggerExit(Collider other){
+			//when the player de-triggers the collider
+		if(other.gameObject.tag == "Player"){
+			enter = false;
+				//resets the Y axis to default, so enemies idle sitting upright (just looks nicer)
+			transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+		}
+	}
+}
---
+++ Lerpz_Battle_Royal/Assets/Scripts/Enemies/spawn0.cs
@@ -0,0 +1,16 @@
+﻿//-JAM
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AddComponentMenu("(Enemy)/Spawner")]
+
+public class spawn0 : MonoBehaviour {
+
+public Rigidbody enemy0;
+
+	void Start(){	
+		Rigidbody clone0;
+			clone0 = Instantiate(enemy0, transform.position, transform.rotation);
+	}
+
+}
