43736493fa5bfcd249af6098c4446a36c7564aa8
Joey <43388262+JoryTheDrunk@users.noreply.github.com>
2018-11-17 14:25:24 -0600

    Uploading/updating VR assets 1

---
+++ Lerpz_Battle_Royal/Assets/LerpzBR/Scripts/CoinController.cs
@@ -0,0 +1,65 @@
+﻿//Cale Toburen
+//11/30/17
+//CSG 114
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public float coinRotation = 3f;
+    public float speed;
+    public float coinMax;
+    public float coinMin;
+    bool moving;
+    bool atMax;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        moving = false;
+        StartCoroutine(Moving());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, coinRotation, 0);
+	}
+    IEnumerator Moving()
+    {
+        moving = false;
+        while (moving == false)
+        {
+            transform.position = transform.position + new Vector3(0, speed * Time.deltaTime, 0);
+            //print(Time.deltaTime);
+            if (transform.position.y >= coinMax)
+            {
+                moving = true;
+            }
+            yield return null;
+        }
+        while (moving == true)
+        {
+            transform.position = transform.position + new Vector3(0, -speed * Time.deltaTime, 0);
+            if (transform.position.y <= coinMin)
+            {
+                moving = false;
+            }
+            yield return null;
+        }
+        StartCoroutine(Moving());
+        yield return null;
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Lerpz_Battle_Royal/Assets/LerpzBR/Scripts/WeaponSwapping.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwapping : MonoBehaviour {
+
+	private GameObject[] weapons;
+
+	public int currentWeapon = 0;
+
+	bool canSwitch = true;
+
+    void Awake()
+    {
+        weapons = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            weapons[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		SwitchWeapon ();
+	}
+
+	void SwitchWeapon() {
+		if (Input.GetKey (KeyCode.Tab) && canSwitch == true) {
+			StartCoroutine (CanSwitchWeapons ());
+			currentWeapon++;
+
+			if (currentWeapon >= weapons.Length)
+				currentWeapon = 0;
+
+			Reset ();
+
+			weapons [currentWeapon].SetActive (true);
+
+		
+		}
+	}
+	void Reset(){
+		for (int i = 0; i < weapons.Length; i++) {
+			weapons [i].SetActive (false);
+		}
+	}
+
+	IEnumerator CanSwitchWeapons(){
+		canSwitch = false;
+		yield return new WaitForSeconds (.1f);
+		canSwitch = true;
+	}
+}
